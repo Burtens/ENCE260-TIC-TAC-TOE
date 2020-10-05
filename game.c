b4a0f5fb961e57e4406fbf36460bee1277bf9d0e
Zachary Kaye <zck10@uclive.ac.nz>
2020-10-05 14:40:54 +1300

    Tried implementing a periodic scheduler.

--- game.c
+++ game.c
@@ -1,28 +1,42 @@
+/** @file   game.c
+    @author Samuel Burtenshaw, Zachary Kaye
+    @date   5 October 2020
+    @brief  A simple paper, scissors, rock game.
+
+    @defgroup psr A simple paper, scissors, rock game.
+*/
+
 #include "system.h"
 #include "pacer.h"
 #include "tinygl.h"
+#include "task.h"
 #include "../fonts/font5x7_1.h"
 
 #define PACER_RATE 500
 #define MESSAGE_RATE 10
 
-int main (void)
-{
-    system_init ();
+system_init ();
     
     tinygl_init(PACER_RATE);
     tinygl_font_set(&font5x7_1);
     tinygl_text_speed_set(MESSAGE_RATE);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
     
-    pacer_init (PACER_RATE);
-
-
-    while (1)
-    {
-        pacer_wait();
-
-
-
-    }
+    
+int main (void)
+{
+    task_t tasks[] =
+    { 
+        {.func = display_task, .period = TASK_RATE / DISPLAY_TASK_RATE,
+         .data = 0},
+        {.func = navswitch_task, .period = TASK_RATE / NAVSWITCH_TASK_RATE,
+         .data = 0},
+        {.func = game_task, .period = TASK_RATE / GAME_TASK_RATE,
+         .data = 0},
+    };
+    
+    system_init ();
+    
+    task_schedule (tasks, 3);
+    return 0;
 }
