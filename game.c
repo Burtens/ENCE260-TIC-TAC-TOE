65e371c8284092d7d8ba1ae5b3cc2c19b668075e
Samuel Burtenshaw <sgb79@uclive.ac.nz>
2020-10-05 22:43:25 +1300

    Testing Button presses with Task_scheduler

--- game.c
+++ game.c
@@ -22,18 +22,25 @@ static void updateScreen(void) {
     tinygl_update ();
 }
 
+static void checkResponse(void) {
+    navswitch_update();
+    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+        tinygl_clear();
+        tinygl_draw_char('5',tinygl_point(0,0));
+    }
+}
+
 int main (void)
 {
     
     system_init ();
     navswitch_init ();
     ir_uart_init ();
-    //pacer_init (LOOP_RATE);
     
     // Initialise tinygl and apply preferences.
-    tinygl_init(250);
+    tinygl_init(TASK_RATE/250);
     tinygl_font_set(&font5x7_1);
-    tinygl_text_speed_set(MESSAGE_RATE);
+    tinygl_text_speed_set(1);
     tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
     
     // Display startup message
@@ -41,9 +48,10 @@ int main (void)
 
     task_t tasks[] =
     {
-            {.func = (task_func_t) updateScreen, .period = TASK_RATE /500.}
+            {.func = (task_func_t) updateScreen, .period = TASK_RATE /500},
+            {.func = (task_func_t) checkResponse, .period = TASK_RATE /100}
     };
 
-    task_schedule(tasks, 1);
+    task_schedule(tasks, 2);
     return 0;
 }
