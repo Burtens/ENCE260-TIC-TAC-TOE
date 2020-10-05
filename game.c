15b36b9b8fe2c144a7e8880f59eee0d02e7a391b
Zachary Kaye <zck10@uclive.ac.nz>
2020-10-05 14:05:51 +1300

    Added init. statements

--- game.c
+++ game.c
@@ -1,12 +1,26 @@
 #include "system.h"
+#include "pacer.h"
+#include "tinygl.h"
+#include "../fonts/font5x7_1.h"
+
+#define PACER_RATE 500
+#define MESSAGE_RATE 10
 
 int main (void)
 {
     system_init ();
+    
+    tinygl_init(PACER_RATE);
+    tinygl_font_set(&font5x7_1);
+    tinygl_text_speed_set(MESSAGE_RATE);
+    tinygl_text_mode_set(TINYGL_TEXT_MODE_SCROLL);
+    
+    pacer_init (PACER_RATE);
 
 
     while (1)
     {
+        pacer_wait();
 
 
 
